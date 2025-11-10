4a925a59248efcdee32f663d4938dd83087d3242
Faishal Ahmad Rafi <farmoron@gmail.com>
2025-11-10 15:40:14 +0700

    Update 1

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -5,7 +5,13 @@ public class Contoh : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int Number1 = 10;
+        int Number2 = 7;
+        int temp = Number1;
+        Number1 = Number2;
+        Number2 = temp;
+        Debug.Log(Number1);
+        Debug.Log(Number2);
     }
 
     // Update is called once per frame
